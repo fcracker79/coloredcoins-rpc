92cabdc5f0064cabd9e348ac6063903aeee333c4
Oded Leiba <oded@colu.co>
2016-03-06 16:02:02 +0200

    support form parameters

--- index.js
+++ index.js
@@ -18,13 +18,35 @@ function handleResponse (err, response, body, cb) {
   cb(null, body)
 }
 
-ColoredCoins.prototype.get = function (method, params, cb) {
-  // var params_string = qs.stringify(params)
-  var params_string = ''
-  for (var key in params) {
-    var value = params[key]
-    params_string += '/' + value
+function buildForm (params) {
+  var form = ''
+  if (params && Object.keys(params).length) {
+    form += '?'
+    var firstOptional = true
+    Object.keys(params).forEach(function (key) {
+      var value = params[key]
+      if (!firstOptional) {
+        form += '&'
+      }
+      form += (key + '=' + value)
+      firstOptional = false
+    })
   }
+  return form
+}
+
+ColoredCoins.prototype.get = function (method, params, formParams, cb) {
+  if (typeof formParams === 'function') {
+    cb = formParams
+    formParams = null
+  }
+  
+  var params_string = ''
+  Object.keys(params).forEach(function (key) {
+    var value = params[key]
+    params_string += '/' + value    
+  })
+  params_string += buildForm(formParams)
   var path = this.ccPath + '/' + method + params_string
   request.get(path, function (err, response, body) {
     handleResponse(err, response, body, cb)
--- test/test.js
+++ test/test.js
@@ -1,5 +1,58 @@
-describe('Test', function () {
-  it('Should do nothing', function (done) {
-    done()
+// describe('Test', function () {
+//   it('Should do nothing', function (done) {
+//     done()
+//   })
+// })
+
+var get = function (method, params, formParams, cb) {
+  if (typeof formParams === 'function') {
+    cb = formParams
+    formParams = null
+  }
+  
+  var params_string = ''
+  Object.keys(params).forEach(function (key) {
+    var value = params[key]
+    params_string += '/' + value    
   })
+  params_string += buildForm(formParams)
+  var path = this.ccPath + '/' + method + params_string
+  cb(null, path)
+}
+
+function buildForm (params) {
+  var form = ''
+  console.log('buildForm #1')
+  if (params && Object.keys(params).length) {
+  	console.log('buildForm #2, params.length  = ', params.length)
+    form += '?'
+    var firstOptional = true
+    Object.keys(params).forEach(function (key) {
+      var value = params[key]
+      if (!firstOptional) {
+        form += '&'
+      }
+      form += (key + '=' + value)
+      firstOptional = false
+    })
+  }
+  return form
+}
+
+var params1 = {
+	utxo: '1234:0',
+	txid : '5678'
+}
+var params2 = {
+	verbosity: '0'
+}
+get('assetmetadata', params1, params2, function (err, res) {
+	console.log('res = ', res)
 })
+get('assetmetadata', params1, function (err, res) {
+	console.log('res = ', res)
+})
+
+
+
+
